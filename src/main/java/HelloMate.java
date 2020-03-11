589179bac3400aa9efecd5055f027fcee8ea8d32
Максим Матуляк <maksymmatuliak@gmail.com>
2020-03-11 23:34:35 +0200

    second hello for mate or pull requests

---
+++ src/main/java/HelloMate.java
@@ -0,0 +1,8 @@
+public class HelloMate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Mate");
+        System.out.println("Its like second hello mate, not in master");
+    }
+
+}
