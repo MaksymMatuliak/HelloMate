f802e87f311437f281a598526c1f2b4f5989e7f9
Максим Матуляк <maksymmatuliak@gmail.com>
2020-03-12 00:09:09 +0200

    Someone added something

--- src/main/java/HelloMate.java
+++ src/main/java/HelloMate.java
@@ -3,6 +3,8 @@ public class HelloMate {
     public static void main(String[] args) {
         System.out.println("Hello Mate");
         System.out.println("Its like second hello mate, not in master");
+
+        //someone add something
     }
 
 }
